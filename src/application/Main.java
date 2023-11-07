33e006f436654c72ac2a8c4814a4824d373785f3
NicacioPereira <vd829020@gmail.com>
2023-11-07 19:47:15 -0300

    Department entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,12 @@
 package application;
 
+import model.entities.Department;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Department obj = new Department(1, "books");
+        System.out.println(obj);
     }
 }
