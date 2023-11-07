8c61928e572c64fd60f001677e4a80f6e557353f
NicacioPereira <vd829020@gmail.com>
2023-11-07 20:06:50 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,18 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
         Department obj = new Department(1, "books");
-        System.out.println(obj);
+
+        Seller seller = new Seller(77,"nicacio" , "nicacio@gmail.com" , new Date(),3400.0 ,obj);
+        System.out.println(seller);
+
     }
 }
